8f4ecf2d53c6bf90393dd3254a55fc9bebca532b
david <neymardavid21@gmail.com>
2020-02-21 12:54:44 +0300

    added contact info

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,13 @@ $(document).ready(function() {
     $("#product-management").show();
   });
 });
+$("#submit").click(function(){
+  var name = $("#contactName").val();
+  var email = $("#contactEmail").val();
+  var info = $("#textArea").val();
+  $(".contactName").text(name);
+  $(".contactEmail").text(email);
+  $(".textArea").text(info);
+  $("#output").show();
+  alert("Thank you " + name + " for your feedback. We will be in touch via " +email);
+});
